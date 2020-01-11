f6fbc1478310767e72f6a177eb9b0dd7281758ba
Charlie Root <root@shrike.shrike-systems.com>
2020-01-11 20:16:56 +0100

    pelicanconf

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,15 +5,10 @@ SITEURL = 'http://www.shrike-systems.com'
 SITENAME = 'Shrike Systems'
 TIMEZONE = 'CET'
 THEME = '../pelican-themes/zurb-F5-basic'
-GOOGLE_ANALYTICS = 'UA-50092958-1'
 DELETE_OUTPUT_DIRECTORY = True
 FEED_ATOM = 'atom.xml'
 FEED_RSS = 'rss.xml'
 TWITTER_USERNAME = 'bjg'
 FLATTR_USER = 'ben.gras'
 STATIC_PATHS = [ 'images' , 'files' ]
-
-PLUGINS = [
-    'pelican_youtube',
-]
 
