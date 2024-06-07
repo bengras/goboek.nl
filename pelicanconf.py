1e3b6fb49ec7ea6ada0649a26759172adb67ea6c
Ben Gras <beng@shrike-systems.com>
2024-06-07 14:02:29 +0200

    m

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,13 +1,16 @@
+import locale
+locale.setlocale(locale.LC_TIME, 'en_US.UTF-8')  # Set to English locale
+
+DEFAULT_LANG = 'en'
 AUTHOR = 'Ben Gras'
-OUTPUT_PATH = '/usr/local/www/apache24/data/www.shrike-systems.com'
+OUTPUT_PATH = '/var/www/www.shrike-systems.com'
 PATH = 'content'
-SITEURL = 'http://www.shrike-systems.com'
+THEME = "/home/beng/pelican-themes/mediumfox"
+SITEURL = 'https://www.shrike-systems.com/'
 SITENAME = 'Shrike Systems'
 TIMEZONE = 'CET'
 DELETE_OUTPUT_DIRECTORY = True
 FEED_ATOM = 'atom.xml'
 FEED_RSS = 'rss.xml'
 TWITTER_USERNAME = 'bjg'
-FLATTR_USER = 'ben.gras'
 STATIC_PATHS = [ 'images' , 'files' ]
-
--- publish.sh
+++ publish.sh
@@ -1,5 +1,8 @@
 set -e
 
+sudo rm -rf /var/www/www.shrike-systems.com
+sudo mkdir  /var/www/www.shrike-systems.com
+sudo chown root  /var/www/www.shrike-systems.com
+sudo chmod 755  /var/www/www.shrike-systems.com
 sudo pelican
-sudo cp keybase.txt /usr/local/www/apache24/data/www.shrike-systems.com/
-
+sudo cp keybase.txt /var/www/www.shrike-systems.com/
