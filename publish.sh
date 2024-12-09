2437bb103bf8272139efd41806791f1816ffe089
Ben Gras <beng@shrike-systems.com>
2024-12-09 09:44:31 +0100

    rm

--- publish.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-sudo rm -rf /var/www/www.shrike-systems.com
-sudo mkdir  /var/www/www.shrike-systems.com
-sudo chown root  /var/www/www.shrike-systems.com
-sudo chmod 755  /var/www/www.shrike-systems.com
-sudo pelican
-sudo cp keybase.txt /var/www/www.shrike-systems.com/
-sudo cp favicon.png /var/www/www.shrike-systems.com/
