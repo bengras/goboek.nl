3b8010ce1c45c78b9a8333b2d707ff688b8d61f6
Ben Gras <beng@shrike-systems.com>
2025-06-24 22:21:49 +0200

    commit

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ set -e
 git add -u . || true
 git commit -mcommit || true
 git pull --rebase
-./venv/bin/pelican content -s pelicanconf.py
+uv run pelican content -s pelicanconf.py
 cp CNAME docs/CNAME
 git add docs
 git commit -mdocs
